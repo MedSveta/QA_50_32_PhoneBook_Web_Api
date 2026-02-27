54fc263d9e70cd485ffc222bda8fbd4776b62fad
MedSveta <sveta1978medved@gmail.com>
2026-02-27 10:16:34 +0200

    TakeScreenShot

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -2,13 +2,19 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import utils.WDListener;
 
 import java.time.Duration;
 
 public class AppManager {
     private WebDriver driver;
+    Logger logger = LoggerFactory.getLogger(AppManager.class);
 
     public WebDriver getDriver() {
         return driver;
@@ -19,6 +25,8 @@ public class AppManager {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        WebDriverListener webDriverListener = new WDListener();
+        driver = new EventFiringDecorator<>(webDriverListener).decorate(driver);
         //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
